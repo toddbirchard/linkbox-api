d11607d0d8e232763876e6209b74516ffdfbd6d9
Todd Birchard <toddbirchard@gmail.com>
2019-04-12 20:59:43 -0400

    new github page

---
+++ _site/terminal/js/main.js
@@ -0,0 +1,58 @@
+$(function() {
+  var data = [
+  {
+    action: 'type',
+    strings: ["curl https://linkbox.link?url=https://example.com/article"],
+    output: $('.mimik-run-output').html(),
+    postDelay: 2000
+  },
+  {
+    action: 'type',
+    strings: ["that was easy!", ''],
+    postDelay: 2000
+  }
+
+];
+  runScripts(data, 0);
+});
+
+function runScripts(data, pos) {
+    var prompt = $('.prompt'),
+        script = data[pos];
+    if(script.clear === true) {
+      $('.history').html('');
+    }
+    switch(script.action) {
+        case 'type':
+          // cleanup for next execution
+          prompt.removeData();
+          $('.typed-cursor').text('');
+          prompt.typed({
+            strings: script.strings,
+            typeSpeed: 20,
+            callback: function() {
+              var history = $('.history').html();
+              history = history ? [history] : [];
+              history.push('$ ' + prompt.text());
+              if(script.output) {
+                history.push(script.output);
+                prompt.html('');
+                $('.history').html(history.join('<br>'));
+              }
+              // scroll to bottom of screen
+              $('section.terminal').scrollTop($('section.terminal').height());
+              // Run next script
+              pos++;
+              if(pos < data.length) {
+                setTimeout(function() {
+                  runScripts(data, pos);
+                }, script.postDelay || 1000);
+              }
+            }
+          });
+          break;
+        case 'view':
+
+          break;
+    }
+}
